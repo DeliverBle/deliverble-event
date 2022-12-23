c1d40ef2340f0a5633c015f887c0b996cca339ee
100Gyeon <bjy9966@naver.com>
2022-12-24 03:52:46 +0900

    fix: 인앱브라우저 처리 다르게 적용

--- src/components/common/BrowserSetter.tsx
+++ src/components/common/BrowserSetter.tsx
@@ -6,7 +6,11 @@ function BrowserSetter() {
     const isIOS = navigator.userAgent.match(/iPhone|iPad/i);
 
     if (agent.indexOf('kakao') > -1 || agent.indexOf('instagram') > -1) {
-      window.close();
+      if (agent.includes('kakao')) {
+        window.location.href = 'kakaotalk://inappbrowser/close';
+      } else if (agent.includes('instagram')) {
+        window.close();
+      }
 
       if (isIOS) {
         window.location.href = 'googlechrome://' + window.location.href.replace(/https?:\/\//i, '');
