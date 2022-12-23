4f41c002d9e405f12f4c4f9a2f1d7b944c5bc861
100Gyeon <bjy9966@naver.com>
2022-12-24 03:23:44 +0900

    fix: 인앱브라우저 닫기

--- src/components/common/BrowserSetter.tsx
+++ src/components/common/BrowserSetter.tsx
@@ -5,16 +5,16 @@ function BrowserSetter() {
     const isInAppBrowser = navigator.userAgent.match(
       /inapp|NAVER|KAKAOTALK|Snapchat|Line|WirtschaftsWoche|Thunderbird|Instagram|everytimeApp|WhatsApp|Electron|wadiz|AliApp|zumapp|iPhone(.*)Whale|Android(.*)Whale|kakaostory|band|twitter|DaumApps|DaumDevice\/mobile|FB_IAB|FB4A|FBAN|FBIOS|FBSS|SamsungBrowser\/[^1]/i,
     );
+    const hrefString = window.location.href;
+    window.close();
 
     if (isInAppBrowser !== null) {
       const isIOS = navigator.userAgent.match(/iPhone|iPad/i);
       if (isIOS) {
-        window.location.href = 'googlechrome://' + window.location.href.replace(/https?:\/\//i, '');
+        window.location.href = 'googlechrome://' + hrefString.replace(/https?:\/\//i, '');
       } else {
         window.location.href =
-          'intent://' +
-          window.location.href.replace(/https?:\/\//i, '') +
-          '#Intent;scheme=http;package=com.android.chrome;end';
+          'intent://' + hrefString.replace(/https?:\/\//i, '') + '#Intent;scheme=http;package=com.android.chrome;end';
       }
     }
   }, []);
