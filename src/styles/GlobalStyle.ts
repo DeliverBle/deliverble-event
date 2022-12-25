b963da0bdf3132a679cf6d53c326a84f3767f352
100Gyeon <bjy9966@naver.com>
2022-12-26 00:59:24 +0900

    design: 디자이너 의견 반영 및 자잘한 스타일링 수정

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -16,7 +16,7 @@ export const GlobalStyle = css`
     display: flex;
     flex-direction: column;
     max-width: 42rem;
-    min-height: 100vh;
+    height: 100%;
     margin: 0 auto;
     padding: 0 2rem;
   }
