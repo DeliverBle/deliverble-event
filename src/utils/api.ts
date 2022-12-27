d69dd0e36e5538bcd145e941dde29cf75fda1db0
HyeJun <solar3070@naver.com>
2022-12-27 22:00:20 +0900

    feat: 사용자 정보 post API fetch 코드 작성

---
+++ src/utils/api.ts
@@ -0,0 +1,30 @@
+import { UserData } from '../components/pages/Home';
+const API_END_POINT = 'https://deliverble.online/event';
+
+export const request = async (url: string, options = {}) => {
+  try {
+    const res = await fetch(`${API_END_POINT}${url}`, {
+      ...options,
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+
+    if (res.status === 201) {
+      return await res.json();
+    }
+    throw new Error('API 처리 오류');
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const createUserData = async (userData: UserData) => {
+  const { data } = await request(`/user`, {
+    method: 'POST',
+    body: JSON.stringify({
+      ...userData,
+    }),
+  });
+  return data;
+};
