f395992570fb12e9be2612cdebb3c8189b378989
100Gyeon <bjy9966@naver.com>
2022-12-28 02:18:54 +0900

    fix: 아이폰에서 로고 깨지는 문제 해결

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,7 +4,9 @@ import { icLogo } from '../../assets/icons';
 function Header() {
   return (
     <StHeader>
-      <img src={icLogo} alt="딜리버블" />
+      <object type="image/svg+xml" data={icLogo}>
+        <img src={icLogo} alt="딜리버블" />
+      </object>
     </StHeader>
   );
 }
@@ -15,8 +17,21 @@ const StHeader = styled.div`
   height: 5.4rem;
   padding: 1.2rem 0;
 
+  & > object {
+    position: relative;
+    z-index: 999;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
   & > img {
     display: block;
     margin: 0 auto;
+    position: relative;
+    z-index: 999;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
 `;
