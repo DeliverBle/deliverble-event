eec48bb00da0854bbb62c338b85f380534b9e2ef
100Gyeon <bjy9966@naver.com>
2022-12-29 02:11:18 +0900

    fix: 인앱 브라우저 해결 시도

--- src/components/common/BrowserSetter.tsx
+++ src/components/common/BrowserSetter.tsx
@@ -1,9 +1,14 @@
+import isWebview from 'is-ua-webview';
 import { useEffect } from 'react';
 
 function BrowserSetter() {
   useEffect(() => {
     const agent = navigator.userAgent.toLowerCase();
     const isIOS = navigator.userAgent.match(/iPhone|iPad/i);
+
+    if (isIOS && isWebview(navigator.userAgent)) {
+      window.location.href = 'googlechrome://' + window.location.href.replace(/https?:\/\//i, '');
+    }
 
     if (agent.indexOf('kakao') > -1 || agent.indexOf('instagram') > -1) {
       if (agent.includes('kakao')) {
@@ -13,7 +18,7 @@ function BrowserSetter() {
       }
 
       if (isIOS) {
-        window.location.href = 'googlechrome://' + window.location.href.replace(/https?:\/\//i, '');
+        alert('크롬 또는 사파리에서 접속해 주세요.');
       } else {
         window.location.href =
           'intent://' +
